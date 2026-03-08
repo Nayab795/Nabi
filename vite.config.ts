9f7c8865e1588c1af8d8359019883c459b8c1508
Your Name <your-email@example.com>
2026-03-08 12:50:20 +0500

    finall commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,27 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/Nabi/',   // ⭐ ADD THIS LINE
+  const env = loadEnv(mode, '.', '');
+  return {
 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+    base: "/Nabi/",   // ⭐ ADD THIS LINE
 
-      plugins: [react()],
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
 
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+    plugins: [react()],
 
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
